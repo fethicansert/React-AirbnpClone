a3e9fed02984bd792ca0ba0f9646f48d9e1e294c
fethi <fethisert0@gmail.com>
2023-11-29 07:46:44 +0200

    some-card-info-added

--- src/card.js
+++ src/card.js
@@ -13,11 +13,15 @@ export default function CardContainer(){
 
 
 function Card(){
+    const name = 'Fethi';
     return (
         <div className="card">
             <img className="card-img" src="https://airbnb-experience-kappa.vercel.app/images/swimmer.png" alt="Katie Zefier Pro Athlete"></img>
             <div className="card-info">
-                <span><i class="fa-solid fa-star"></i></span>
+                <span className="card-info-line"><i class="fa-solid fa-star"></i> 5.0 <span className="lighter-text">(6) . USA</span></span>
+                <span className="card-info-line">Life lessonns with Katie Zaferes</span>
+                <span className="card-info-line"><b>From $136</b> / person</span>
+                <h1>Hello{' '+name}</h1>
             </div>
         </div>
     );
